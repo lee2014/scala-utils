de3ff4ded1ea631b0d2af014f261c011ebd64307
lee2014 <lee@umeng.com>
2017-03-29 13:16:47 +0800

    update FlajoletMartin

--- src/main/scala/algorithm/FlajoletMartin.scala
+++ src/main/scala/algorithm/FlajoletMartin.scala
@@ -1,36 +1,48 @@
 package algorithm
 
 import scala.math._
-import scala.util.control.Breaks._
 
 /**
   * Description: https://en.wikipedia.org/wiki/Flajolet%E2%80%93Martin_algorithm
   * Created by lee on 17-3-20.
   */
-class FlajoletMartin extends CardinalityEstimation with MD5Hash {
+class FlajoletMartin(private val p: Int, private val L: Int)
+  extends CardinalityEstimation with MD5Hash {
 
-  private final var bitmap = BigInt(0)
+  require(p > 0 && L > 0)
+
+  private[algorithm] val m: Int = pow(2, p).toInt
+
+  private final val bitmaps = Array.fill[BigInt](m)(BigInt(0))
 
   private final val factor = 0.77351
 
-  private[algorithm] def leastSignificant(value: Any): Int = hash(value).lowestSetBit
+  private[algorithm] def rho(value: BigInt): Int = value.lowestSetBit
 
   private[algorithm] override def insert(value: Any): Unit = {
-    bitmap = bitmap | BigInt(pow(2, leastSignificant(value)).toLong)
+    val hashValue = hash(value).abs
+    val mapIdx = (hashValue % m).toInt
+    val index = rho(hashValue / m)
+
+    bitmaps(mapIdx) = bitmaps(mapIdx) | BigInt(pow(2, index).toLong)
   }
 
+  /**
+    * E := nmap / (q * (1 + 0.31 / nmap ))*2**(S/nmap)
+    */
   override def estimate: Long = {
-    var R: Int = bitmap.bitLength
+    var S: Double = 0
 
-    breakable {
-      Range(0, bitmap.bitLength).foreach { i =>
-        if (!bitmap.testBit(i)) {
-          R = i
-          break
-        }
-      }
+    for (i <- 0 until m) {
+      var R = 0
+      while (R <= i && bitmaps(i).testBit(R) && R < L) R += 1
+      S += R
     }
 
-    (pow(2, R) / factor).toLong
+    (m * pow(2, S / m) / (factor * (1 + 0.31 / m))).toLong
   }
+}
+
+object FlajoletMartin {
+  def apply(n: Int, l: Int): FlajoletMartin = new FlajoletMartin(n, l)
 }
---
+++ src/test/scala/algorithm/CardinalityEstimationTest.scala
@@ -0,0 +1,60 @@
+package algorithm
+
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+import scala.math.{abs, sqrt}
+import scala.util.Random
+
+/**
+  * Created by lee on 17-3-29.
+  */
+class CardinalityEstimationTest extends FunSuite with BeforeAndAfterAll{
+  test("test FlajoletMartin") {
+    val flajoletMartin = FlajoletMartin(10, 64)
+
+    val random = new Random(System.nanoTime)
+    val tests = Range(0, 10000000).map(_ => random.nextInt(200000))
+
+    flajoletMartin.insertAll(tests)
+    val probabilityCount = flajoletMartin.estimate
+    val realCount = tests.distinct.size
+
+    val error = abs(probabilityCount.toDouble - realCount) / realCount
+
+    println(probabilityCount)
+    println(s"Error: $error")
+    println(s"Expectation: ${0.78 / sqrt(flajoletMartin.m)}")
+  }
+
+  test("test HyperLogLogPlusPlus") {
+    val hyperLogLog = HyperLogLogPlusPlus(16)
+
+    val random = new Random(System.nanoTime)
+    val tests = Range(0, 10000000).map(_ => random.nextInt(200000))
+
+    hyperLogLog.insertAll(tests)
+    val probabilityCount = hyperLogLog.estimate
+    val realCount = tests.distinct.size
+
+    val error = (probabilityCount.toDouble - realCount) / realCount
+
+    println(s"Error: $error")
+    println(s"Expectation: ${1.04 / sqrt(hyperLogLog.m)}")
+  }
+
+  test("test LogLog") {
+    val loglog = LogLog(16)
+
+    val random = new Random(System.nanoTime)
+    val tests = Range(0, 10000000).map(_ => random.nextInt(200000))
+
+    loglog.insertAll(tests)
+    val probabilityCount = loglog.estimate
+    val realCount = tests.distinct.size
+
+    val error = (probabilityCount.toDouble - realCount) / realCount
+
+    println(s"Error: $error")
+    println(s"Expectation: ${1.3 / sqrt(loglog.m)}")
+  }
+}
